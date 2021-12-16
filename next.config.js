8ac4fd09a67f0c30e9e92c01507cb99f69bce467
JAOUAD ICHI <ichijawad@gmail.com>
2021-12-16 16:49:52 +0100

    pwa feature added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: ['image.tmdb.org'],
   },
-}
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
