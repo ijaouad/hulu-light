5cbc87b9638a74de41f56984d30455d7cf80c3eb
JAOUAD ICHI <ichijawad@gmail.com>
2021-12-16 13:52:08 +0100

    error fixed

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -6,7 +6,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
     const BASE_URL = 'https://image.tmdb.org/t/p/original'
     return (
         <div ref={ref} className="p-3 group cursor-pointer transition duration-200 ease-in sm:hover:scale-105 hover:z-50">
-            <Image src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} layout="responsive" width={1920} height={1080} />
+            <Image src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} layout="responsive" width={1920} height={1080} alt=""/>
             <div className="p-2">
                 <p className="truncate max-w-md">{result.overview}</p> 
 
@@ -21,5 +21,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
         </div>
     )
 })
+
+Thumbnail.displayName = 'Thumbnail';
 
 export default Thumbnail
